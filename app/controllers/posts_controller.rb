1cee6d40c054792c358c313ca980362312f5104e
lshapz <laurashapiro01@gmail.com>
2016-10-22 14:13:49 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,21 @@
 class PostsController < ApplicationController
+    #before_action :set_post!, only: [:show, :edit, :update]
+
   def show
     @post = Post.find(params[:id])
+  end
+  def new 
+    @post = Post.new
+  end 
+
+  def create
+    @post = Post.new
+    if @post.valid? 
+      @post.save 
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -8,16 +23,21 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
+  @post = Post.find(params[:id])
 
-    @post.update(post_params)
-
+  if  @post.update(post_params)
     redirect_to post_path(@post)
+  else
+    render :edit
+  end
   end
 
   private
 
   def post_params
     params.permit(:title, :category, :content)
+  end
+    def set_post!
+    @post = Post.find(params[:id])
   end
 end
